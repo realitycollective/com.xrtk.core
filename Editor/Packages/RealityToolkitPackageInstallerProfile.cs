dcbc2a7903ff15f99f82a28b89a7522117e8d01a
Dino Fejzagić <dino.f@live.de>
2024-06-20 09:30:51 +0200

    Define toolkit package installer profile

---
+++ Editor/Packages/RealityToolkitPackageInstallerProfile.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Reality Collective. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using RealityCollective.ServiceFramework.Editor.Packages;
+
+namespace RealityToolkit.Editor.Packages
+{
+    /// <summary>
+    /// This profile identifies a package as a toolkit package and is reserved for future use,
+    /// where we will implement a installation window listing all found toolkit packages, that may be installed
+    /// into the active service configuration.
+    /// </summary>
+    public class RealityToolkitPackageInstallerProfile : PackageInstallerProfile { }
+}
