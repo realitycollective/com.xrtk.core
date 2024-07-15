894f43b191fbc591bd2387e881dc8cf2b1494ad7
Dino Fejzagić <dino.f@live.de>
2024-07-15 09:03:04 +0200

    Fix comments

--- Runtime/Input/InteractionBehaviours/InteractorTrackingMode.cs
+++ Runtime/Input/InteractionBehaviours/InteractorTrackingMode.cs
@@ -4,16 +4,16 @@
 namespace RealityToolkit.Input.InteractionBehaviours
 {
     /// <summary>
-    /// Supported interactor tracking modes.
+    /// Supported <see cref="Interactors.IInteractor"/> tracking modes.
     /// </summary>
     public enum InteractorTrackingMode
     {
         /// <summary>
-        /// We are tracking the interactors position to determine lever movement.
+        /// We are tracking the interactors position upon interaction.
         /// </summary>
         Position = 0,
         /// <summary>
-        /// We are tracking the interactors rotation to determine lever movement.
+        /// We are tracking the interactors rotation upon interaction.
         /// </summary>
         Rotation
     }
